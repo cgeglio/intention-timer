90bdc50abdf5a70f2853b8289babe8f8e72b22ab
Virginia Ladd <virginialadd2@gmail.com>
2019-10-07 22:42:29 -0600

    Add color when icon is clicked

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 
+
+function activeStudy() {
+  var pic = document.querySelector('.study');
+  pic.src = "assets/study-active.svg";
+  pic.classList.add(".li-study");
+}
+
+
+// 
+// function activeMeditate() {
+//   var pic = document.querySelector('.meditate');
+//   pic.src = "assets/meditate-active.svg";
+//   pic.classList.add(".li-meditate");
+//
+//
+// function activeExercise() {
+//   var pic = document.querySelector('.exercise');
+//   pic.src = "assets/exercise-active.svg";
+//   // pic.classList.add(".li-study");
+// }
+
+document.querySelector('.study').addEventListener('click', activeStudy);
+document.querySelector('.meditate').addEventListener('click', activeMeditate);
+document.querySelector('.exercise').addEventListener('click', activeExercise);
