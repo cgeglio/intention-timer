3ea92ae03a56be7002d71748ff404c11892f9c59
Virginia Ladd <virginialadd2@gmail.com>
2019-10-08 08:42:29 -0600

    Fix color functions of code

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ function activeStudy() {
 }
 
 
-// 
-// function activeMeditate() {
-//   var pic = document.querySelector('.meditate');
-//   pic.src = "assets/meditate-active.svg";
-//   pic.classList.add(".li-meditate");
-//
-//
-// function activeExercise() {
-//   var pic = document.querySelector('.exercise');
-//   pic.src = "assets/exercise-active.svg";
-//   // pic.classList.add(".li-study");
-// }
+
+function activeMeditate() {
+  var pic = document.querySelector('.meditate');
+  pic.src = "assets/meditate-active.svg";
+  pic.classList.add(".li-meditate");
+}
+
+function activeExercise() {
+  var pic = document.querySelector('.exercise');
+  pic.src = "assets/exercise-active.svg";
+  // pic.classList.add(".li-study");
+}
 
 document.querySelector('.study').addEventListener('click', activeStudy);
 document.querySelector('.meditate').addEventListener('click', activeMeditate);
