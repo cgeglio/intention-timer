8b6b367084d557c56c2b352eb718df57017fbaf6
Virginia Ladd <virginialadd2@gmail.com>
2019-10-09 14:53:29 -0600

    Syncs color of timer to activity

--- main.js
+++ main.js
@@ -1,49 +1,58 @@
-// var picS = document.querySelector('.study');
-// var picM = document.querySelector('.meditate');
-// var picE = document.querySelector('.exercise');
-
+//Activate Icons
 function activeStudy() {
   var pic = document.querySelector('.study');
   var select = document.querySelector('.study2');
+  var timerBtn = document.querySelector("#start");
+  var titleHeader = document.querySelector("output");
+  var timerHeader = document.querySelector("h4");
   pic.src ="assets/study-active.svg";
   pic.classList.add("li-study");
   pic.classList.remove("study");
   select.classList.add("li-study");
   select.classList.remove("study");
+  timerBtn.classList.add("timerStudy");
+  titleHeader.classList.add("timerHeader")
+  timerHeader.classList.add("timerHeader")
 }
 
 function activeMeditate() {
   var pic = document.querySelector('.meditate');
   var select = document.querySelector('.meditate2');
+  var timerBtn = document.querySelector("#start");
+  var titleHeader = document.querySelector("output");
+  var timerHeader = document.querySelector("h4");
   pic.src = "assets/meditate-active.svg";
   pic.classList.add("li-meditate");
   pic.classList.remove("meditate");
   select.classList.add("li-meditate");
   select.classList.remove("meditate");
-
-  console.log(pic)
+  timerBtn.classList.add("timerMeditate");
+  titleHeader.classList.add("timerHeader")
+  timerHeader.classList.add("timerHeader")
 }
 
 function activeExercise() {
   var pic = document.querySelector('.exercise');
   var select = document.querySelector('.exercise2');
+  var timerBtn = document.querySelector("#start");
+  var titleHeader = document.querySelector("output");
+  var timerHeader = document.querySelector("h4");
   pic.src = "assets/exercise-active.svg";
   pic.classList.add("li-exercise");
   pic.classList.remove("exercise");
   select.classList.add("li-exercise");
   select.classList.remove("exercise");
+  timerBtn.classList.add("timerExercise");
+  titleHeader.classList.add("timerHeader")
+  timerHeader.classList.add("timerHeader")
 }
-
-// console.log(activeExercise);
-
-document.querySelector('.study').addEventListener('click', activeStudy);
 
 document.querySelector('.study').addEventListener('click', activeStudy);
 document.querySelector('.meditate').addEventListener('click', activeMeditate);
 document.querySelector('.exercise').addEventListener('click', activeExercise);
 
 
-
+//Timer Set Up
 var setTime = document.querySelector('button');
 
 function hideForm () {
